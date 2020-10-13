eae79d06054e35e2f49403d1c32d47f532fa8512
Hayden-Neilson <haydenneilson5@gmail.com>
2020-10-12 19:59:54 -0600

    home html

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return ("website content goes here")
+    return (render_template("home.html"))
 
 
-@app.route("/")
+@app.route("/about/")
 def about():
-    return("About content goes here")
+    return(render_template("about.html"))
 
 
 if __name__ == "__main__":
